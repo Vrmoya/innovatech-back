f2d829b96374fa0edf62992689daab58367c73f3
victormoya <viclauti@gmail.com>
2024-02-16 01:13:29 -0300

    correcciones en authController y creacion de isAdmin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 const AuthController = require('../controllers/AuthController');
-// const productos = require('../../tech.json');
-
 const  findAllProducts = require('../controllers/findAllProducts');
 const getProductById = require('../controllers/getProductById');
 const getProductByModel = require('../controllers/getProductByModel');
+const isAdmin = require('../middlewares/isAdmin');
 
 //Ruta para obtener todos los productos
 router.get('/products', findAllProducts);
 router.get('/products/:id', getProductById);
 router.get('/products/model', getProductByModel);
 
+//Ruta para el registro de administradores
 
+router.post('/api/admin/register', isAdmin, AuthController.signUpAdmin);
 
 // Rutas para el inicio de sesión y el registro con correo electrónico y contraseña
 router.post('/api/signin', AuthController.signIn);
