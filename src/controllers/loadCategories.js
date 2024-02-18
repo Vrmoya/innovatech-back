238f26e957667f90a59e2136f479e67c441315f0
Vrmoya <viclauti@gmail.com>
2024-02-18 19:20:11 -0300

    correccion de loadcategories

--- src/controllers/loadCategories.js
+++ src/controllers/loadCategories.js
@@ -1,12 +1,18 @@
 const { Categories } = require("../db")
 
-
 const categories = [{ name: 'tablet' }, { name: 'laptop' }, { name: 'smartphone' }, { name: 'headphone' }, { name: 'keyboard' },]
 
 const loadCategories = async () => {
     try {
-        // Insertar los categorias en la base de datos
-        await Categories.bulkCreate(categories);
+        for (let category of categories) {
+            // Busca la categoría en la base de datos
+            let existingCategory = await Categories.findOne({ where: { name: category.name } });
+
+            // Si la categoría no existe, la crea
+            if (!existingCategory) {
+                await Categories.create(category);
+            }
+        }
         console.log('Categorias cargadas exitosamente');
     } catch (error) {
         console.error('Error al cargar los categorias:', error);
