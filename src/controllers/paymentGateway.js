9383fe240a380ef89c89683f8315ebecac88ad3c
assaffmatias <matias.assaff98@gmail.com>
2024-02-26 17:45:02 -0300

    payment action modified

--- src/controllers/paymentGateway.js
+++ src/controllers/paymentGateway.js
@@ -4,32 +4,33 @@ const { MercadoPagoConfig, Preference } = require('mercadopago')
 
 const client = new MercadoPagoConfig({ accessToken: `${ACCESS_TOKEN}` });
 
-const paymentGateway =
-    async (req, res) => {
-        console.log(req.body);
-        try {
-            const body = {
-                items: [
-                    {
-                        title: req.body.title,
-                        quantity: Number(req.body.quantity),
-                        unit_price: Number(req.body.price),
-                        currency_id: "ARS",
-                    }
-                ],
-                back_urls: {
-                    "success": "http://localhost:5173/",
-                    "failure": "http://localhost:5173/",
-                    "pending": "http://localhost:5173/"
-                },
-                auto_return: "approved",
-            };
-            const preference = new Preference(client);
-            const result = await preference.create({ body });
-            res.json({ id: result.id })
-        } catch (error) {
-            console.log(error);
-        }
+paymentGateway = async (req, res) => {
+    console.log(req.body);
+    try {
+        const itemsFromFrontend = req.body.items;
+        const itemsForBackend = itemsFromFrontend.map(item => ({
+            title: item.title,
+            quantity: item.quantity,
+            unit_price: item.price,
+            // currency_id: "ARS",
+        }));
+
+        const body = {
+            items: itemsForBackend,
+            total: req.body.total,
+            back_urls: {
+                "success": "http://localhost:5173/",
+                "failure": "http://localhost:5173/",
+                "pending": "http://localhost:5173/"
+            },
+            auto_return: "approved",
+        };
+        const preference = new Preference(client);
+        const result = await preference.create({ body });
+        res.json({ id: result.id });
+    } catch (error) {
+        console.log(error);
     }
+}
 
 module.exports = paymentGateway;
