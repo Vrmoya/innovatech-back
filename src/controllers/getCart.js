b5ed0a512c9efa240bb34099c12d931d904fec2d
Nicolas Buszczak <nico_947quil@hotmail.com>
2024-03-14 19:54:49 -0300

    Fix rutas carrito

--- src/controllers/getCart.js
+++ src/controllers/getCart.js
@@ -3,18 +3,20 @@ const { Cart, CartItem } = require('../db')
 
 const getCart = async (req, res) => {
     try {
-        const response = await Cart.findAll();
-        const data = response.forEach(async element => {
-            const idElement = element.id
-            element.Items = await CartItem.findAll({
-                where: {
-                    cartId: {
-                        [Op.iLike]: `%${idElement}%`,
-                    },
-                },
-            })
-        });
-        res.status(200).json(data);
+        const response = await Cart.findAll({ where: { payment_status: 'approved' }, include: [{ model: CartItem, as: 'cartItems' }] });
+        // const data = response.forEach(async element => {
+        //     // console.log(response);
+        //     const idElement = element.id
+        //     element.Items = await CartItem.findAll({
+        //         where: {
+        //             cartId: {
+        //                 [Op.iLike]: `%${idElement}%`,
+        //             },
+        //         },
+        //     })
+        // });
+        // res.status(200).json(data);
+        res.status(200).json(response);
     } catch (error) {
         console.error('Error al obtener los datos:', error);
         res.status(500).json({ message: 'Error al obtener los datos' });
--- src/controllers/getCartById.js
+++ src/controllers/getCartById.js
@@ -4,25 +4,27 @@ const { Cart, CartItem } = require('../db')
 const getCartById = async (req, res) => {
     try {
         const { id } = req.params;
-        const response = await Cart.findAll({
-            where: {
-                userId: {
-                    [Op.iLike]: `%${id}%`,
-                },
-            },
-        });
+        const response = await Cart.findAll({ where: { userId:id, payment_status: 'approved' }, include: [{ model: CartItem, as: 'cartItems' }] });
+        // const response = await Cart.findAll({
+        //     where: {
+        //         userId: {
+        //             [Op.iLike]: `%${id}%`,
+        //         },
+        //     },
+        // });
 
-        const data = response.forEach(async element => {
-            const idElement = element.id
-            element.Items = await CartItem.findAll({
-                where: {
-                    cartId: {
-                        [Op.iLike]: `%${idElement}%`,
-                    },
-                },
-            })
-        });
-        res.status(200).json(data);
+        // const data = response.forEach(async element => {
+        //     const idElement = element.id
+        //     element.Items = await CartItem.findAll({
+        //         where: {
+        //             cartId: {
+        //                 [Op.iLike]: `%${idElement}%`,
+        //             },
+        //         },
+        //     })
+        // });
+        // res.status(200).json(data);
+        res.status(200).json(response);
     } catch (error) {
         console.error('Error al obtener los datos:', error);
         res.status(500).json({ message: 'Error al obtener los datos' });
